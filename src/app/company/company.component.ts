0e613a5265d39af19cbe76dd392388cf1b25297d
Vishwani Patel <vishwanipatel494@gmail.com>
2022-10-18 16:44:31 +0530

    feat: add and edit data

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import { company } from './company.model';
+import { DataTransferService } from './service/data-transfer.service';
 import { CompanyService } from './service/company.service';
 
 @Component({
@@ -8,16 +9,26 @@ import { CompanyService } from './service/company.service';
   styleUrls: ['./company.component.scss']
 })
 export class CompanyComponent implements OnInit {
-  public dbcompanyList: company[];
-  
+ 
 
-  constructor(private companyService: CompanyService) {
+
+  public dbcompanyList: company[];
+
+
+
+  constructor(private companyService: CompanyService,
+    private dataTransfer:DataTransferService) {
     this.dbcompanyList = [];
+
   }
 
   ngOnInit(): void {
+    this.dataTransfer.communicationData.subscribe((data)=>{
+      if(data){
+        this.getAllCompany();
+      }
+    })
     this.getAllCompany();
-
   }
   getAllCompany() {
     this.companyService.getCompany().subscribe((company: company[]) => {
@@ -25,8 +36,13 @@ export class CompanyComponent implements OnInit {
     })
   }
   deleteCompanyDetails(id:number){
+    if (confirm('Are you sure to delete this company?')){
     this.companyService.deleteCompany(id).subscribe((Response) => {
       this.getAllCompany();
     })
+
   }
-}
+  }
+ 
+
+  }
