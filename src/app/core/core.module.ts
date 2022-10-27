6f5990127240934b26e9ca7e5298ab7b8ef173a3
Vishwani Patel <vishwanipatel494@gmail.com>
2022-10-27 13:12:51 +0530

    feat: registration page created

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,19 +5,27 @@ import { NgDynamicBreadcrumbModule } from 'ng-dynamic-breadcrumb';
 import { RouterModule } from '@angular/router';
 import { DataTransferService } from '../company/service/data-transfer.service';
 import { LoginComponent } from './Authentication/login/login.component';
+import { RegistrationComponent } from './Authentication/registration/registration.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { UserService } from './Authentication/user.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    LoginComponent
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     CommonModule,
     NgDynamicBreadcrumbModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   exports:[
     HeaderComponent,
   ],
+  providers: [UserService ]
 })
 export class CoreModule { }
